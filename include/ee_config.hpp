2f9d8e968ae73ab56798fd05c3710b1196dccc2f
dm-rep <david.muller.cz@gmail.com>
2023-01-11 23:10:25 +0100

    fix bug

--- include/ee_config.hpp
+++ include/ee_config.hpp
@@ -2,39 +2,100 @@
 #define _ee_config_h
 
 #include <stdint.h>
-#include <functional>
 #include <EEPROM.h>
 
-namespace dmHelper
+template<typename T> class EepromConfig
 {
-	// Class providing save/load/restore configuration from/to eeprom
-	template<typename T> class EepromConfig
-	{
-		public:
-			// data member
-			T data;	
-			// calls before using (from setup), returns false when the default was restored
-			// version number reflects the T structure (change it on change T)
-			bool init(uint16_t version);
-			// loads from eeprom (FALSE when the default was restored - version disagreement)
-			bool load();
-			// restores to default
-			void loadDefault();
-			// save to eeprom
-			bool save();
-			// check validity
-			bool isValidStructure();
-			// if the saved content is different from the current (need to be saved)
-			bool isDirty();
-			// read from address (for debuging)
-			uint8_t readByte(uint16_t add);
-			// method to get the default
-			std::function<void()> fnObtainDefault;
-		private:
-			uint16_t m_ver;
-			uint32_t m_hash;
-			uint32_t calcHash();
-	};
-}
+    public:
+		// configuration data (type T)
+		T data;
+		
+		// init (calls from setup) return false when the default was restored
+		// version: identifier of the data structure (change on change!)
+		bool init(uint16_t version)
+		{
+			m_ver = version;
+			EEPROM.begin(sizeof(T) + 2);
+			return load();
+		}
+
+		// reads config (returns false when default had to be loaded)
+		bool load()
+		{
+			bool r = isValidStructure();
+			
+			if (r)
+			{
+				EEPROM.get<T>(2, data);	
+			}
+			else
+			{			
+				loadDefault();
+				save();
+			}
+
+			return r;
+		}
+
+		// restore default
+		void loadDefault()
+		{
+			fnObtainDefault();
+		}	
+
+		// saves current, returns true on success
+		bool save()
+		{
+			//EEPROM.writeUShort(0, m_ver);
+			EEPROM.put(0, m_ver);
+			EEPROM.put<T>(2, data);	
+			bool r = EEPROM.commit();
+			if (r) m_hash = _calcHash();
+			return r;
+		}
+
+		// checks validity
+		bool isValidStructure()
+		{
+			uint16_t x;
+			return EEPROM.get(0, x) == m_ver;
+		}
+
+		// if the saved is different from the current
+		bool isDirty()
+		{
+			return (m_hash != _calcHash());
+		}
+
+		// read from address (debug)
+		uint8_t readByte(uint16_t add)
+		{
+			return EEPROM.read(add);
+		}
+    
+		std::function<void()> fnObtainDefault;
+
+    private:
+        uint16_t m_ver;
+        uint32_t m_hash;
+
+		// calculates simple hash
+		uint32_t _calcHash()
+		{
+			uint16_t len = sizeof(T);
+			uint32_t r = len;
+			char *buff = (char*)&data;
+
+			for (uint16_t i = 0; i < len; i++)
+			{
+				r ^= buff[i] + 0x9e3779b9 + (r << 6) + (r >> 2);
+			}
+			return r;    
+		}
+};
+
+
+
+
 
 #endif
